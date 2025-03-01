362b8c796b1fa171994e0424c5c557451924afaf
zPirataa <134012976+zPirataa@users.noreply.github.com>
2025-03-01 23:18:05 +0000

    Adicionado fila_generica e fila_teste

--- aula2/src/main.rs
+++ aula2/src/main.rs
@@ -1,3 +1,12 @@
+use std::collections::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut lista: LinkedList<i32> = LinkedList::new();
+
+    lista.push_back(5);
+    lista.push_back(15);
+    lista.push_back(25);
+
+    lista.pop_front(); // Remove o primeiro elemento
+    println!("{:?}", lista);
 }
